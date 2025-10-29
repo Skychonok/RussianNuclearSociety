f673168b41ea36375898bd64ae8c15079d3c2722
Skychonok <156518074+Skychonok@users.noreply.github.com>
2025-10-29 15:08:29 +0300

    теперь используем шаблоны

--- main/views.py
+++ main/views.py
@@ -2,14 +2,26 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 
-def main(request):
-    return render(request, 'main/base.html')
+def home_page(request):
+    return render(request, 'main/home.html')
 
-def registration(request):
-    return render(request, 'main/register.html')
+def educ_page(request):
+    return render(request, 'main/educ.html')
 
-def login(request):
+def events_page(request):
+    return render(request, 'main/events.html')
+
+def materials_page(request):
+    return render(request, 'main/materials.html')
+
+def about_us_page(request):
+    return render(request, 'main/about_us.html')
+
+def login_page(request):
     return render(request, 'main/login.html')
 
-def logout(request):
-    return None
+def register_page(request):
+    return render(request, 'main/register.html')
+
+def profile_view(request):
+    return render(request, 'main/profile.html')
