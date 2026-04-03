f905be1f9073dcb842da0e98d7036146c5bd5338
Skychonok <156518074+Skychonok@users.noreply.github.com>
2026-04-03 16:43:31 +0300

    s

--- main/urls.py
+++ main/urls.py
@@ -50,4 +50,11 @@ urlpatterns = [
     path('management/', views.article_management_dashboard, name='management_dashboard'),
     path('management/articles/', views.ArticleManagementListView.as_view(), name='article_management'),
 
+
+    # Password reset
+    path('password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
+    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
+
 ]
